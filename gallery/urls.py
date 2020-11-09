89c2ff57d0d3d35977921c3c20478368e9313ae6
tomito26 <tommybwah@gmail.com>
2020-11-09 18:41:21 +0300

    removed regex from the urls and used django 3 paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'',include('galleryApp.urls'))
+    path('admin/', admin.site.urls),
+    path('',include('galleryApp.urls'))
 ]
--- galleryApp/urls.py
+++ galleryApp/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
 from . import views
-from django.conf import settings
+from django.urls import path
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', views.galler, name='galler'),
-    url(r'^search/', views.search_by_image_category, name='search_category'),
-    url(r'picture/(?P<image_id>\d+)', views.picture, name="picture")
+    path('', views.galler, name='galler'),
+    path('search/', views.search_by_image_category, name='search_category'),
+    path('picture/<int:image_id>/', views.picture, name="picture")
 ]
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
